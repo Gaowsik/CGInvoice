e95f9c9c62dfad7d8e141912c5533da0f5dc40f5
Gaowsik <gaowsikt@sankaraa.com>
2024-08-30 17:46:33 +0530

    set up back4app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.parse.sdk.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
