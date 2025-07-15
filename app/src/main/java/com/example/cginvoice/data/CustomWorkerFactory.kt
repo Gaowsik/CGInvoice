8960dd330e684022507586935a8926c0bfed36a6
Gaowsik <gaowsikt@sankaraa.com>
2025-07-15 10:23:45 +0530

    Refactor

--- app/src/main/java/com/example/cginvoice/data/CustomWorkerFactory.kt
+++ app/src/main/java/com/example/cginvoice/data/CustomWorkerFactory.kt
@@ -4,14 +4,15 @@ import android.content.Context
 import androidx.work.ListenableWorker
 import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
+import com.example.cginvoice.data.repository.client.ClientRepository
 import com.example.cginvoice.data.repository.user.UserRepository
 import javax.inject.Inject
 
-class CustomWorkerFactory @Inject constructor(private val userRepository: UserRepository) :
+class CustomWorkerFactory @Inject constructor(private val userRepository: UserRepository,private val clientRepository: ClientRepository) :
     WorkerFactory() {
     override fun createWorker(
         appContext: Context,
         workerClassName: String,
         workerParameters: WorkerParameters
-    ): ListenableWorker? = SyncDataWorker(userRepository, appContext, workerParameters)
+    ): ListenableWorker? = SyncDataWorker(userRepository, clientRepository,appContext, workerParameters)
 }
